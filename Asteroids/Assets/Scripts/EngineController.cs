2a7c20548593dbaaaae693ccfaa8447316775360
Simeon Tsekov <simeon.1.tsekov@gmail.com>
2020-04-08 08:08:00 +0300

    SC: Added engine particles and tilting on turn.

---
+++ Asteroids/Assets/Scripts/EngineController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineController : MonoBehaviour
+{
+    public ParticleSystem propulsion1;
+    public ParticleSystem propulsion2;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        if (verticalAxis > 0 && horizontalAxis == 0)
+        {
+            propulsion1.Emit(1);
+            propulsion2.Emit(1);
+        } else if (horizontalAxis > 0)
+        {
+            propulsion1.Emit(1);
+        } else if (horizontalAxis < 0)
+        {
+            propulsion2.Emit(1);
+        }
+    }
+}
